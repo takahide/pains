ddd2fdade30eab97e31549eaa05d3994ee229efc
Takahide Hosokawa <takahide@indeed.com>
2014-04-18 00:13:49 +0900

    localとprodの切り替え

--- hi.rb
+++ hi.rb
@@ -4,11 +4,19 @@ require 'active_record'
 require 'mysql2'
 require 'haml'
 
-set :environment, :production
-set :port, 80
+configure :development do
+  require 'sinatra/reloader'
+  register Sinatra::Reloader
+end
+
+configure :production do
+  set :port, 80
+end
 
 ActiveRecord::Base.configurations = YAML.load_file('config/database.yml')
-ActiveRecord::Base.establish_connection('pains_dev')
+
+ActiveRecord::Base.establish_connection('local_dev') if development?
+ActiveRecord::Base.establish_connection('pains_dev') if production?
 
 class Test < ActiveRecord::Base
 end
